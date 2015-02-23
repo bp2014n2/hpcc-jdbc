43063dc2e32335800b8a45be7176655be47b29da
peter.schwarz <peter.schwarz@student.hpi.uni-potsdam.de>
2015-02-23 13:03:17 +0100

    Add missing HPCCDriverProperties file

---
+++ src/connectionManagement/HPCCDriverProperties.java
@@ -0,0 +1,99 @@
+package connectionManagement;
+
+import java.sql.DriverPropertyInfo;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Properties;
+
+public class HPCCDriverProperties extends Properties{
+	private static final long serialVersionUID = 1L;
+	
+	private static final int DEFAULT_VALUE_POSITION = 0;
+	private static final int DESCRIPTION_POSITION = 1;
+	
+	private static final String DEFAULT_SERVER_ADDRESS = "http://localhost";
+	
+	private static HashMap<String, String[]> defaultProperties = new HashMap<String, String[]>(){
+		private static final long serialVersionUID = 1L;
+
+		{
+			put("ConnectTimeoutMilli", 	new String[]{"5000","HPCC requests connection time out value in milliseconds."});
+			put("ReadTimeoutMilli", 	new String[]{"15000","HPCC requests connection read time out value in milliseconds."});
+			put("LazyLoad", 			new String[]{"true","If disabled, all HPCC metadata loaded and cached at connect time; otherwise HPCC file, and published query info is loaded on-demand."});
+			put("EclResultLimit", 		new String[]{"100","Default limit on number of result records returned."});
+			put("TraceLevel", 			new String[]{HPCCJDBCUtils.defaultLogLevel.getName(),	"Logging level (java.util.logging.level)."});
+			put("TraceToFile", 			new String[]{"false","false -> System.out, true -> " + HPCCJDBCUtils.workingDir +  HPCCJDBCUtils.traceFileName});
+			put("TargetCluster", 		new String[]{"hthor","Target cluster on which to execute ECL code."});
+			put("QuerySet", 			new String[]{"hthor","Queryset from which published query (Stored Procedure) is chosen."});
+			put("PageSize", 			new String[]{"100","Number of HPCC data files (DB tables) or HPCC published queries (DB Stored Procs) displayed."});
+			put("PageOffset", 			new String[]{"0","Starting HPCC data file or HPCC published queries displayed."});
+			put("password", 			new String[]{"hpccdemo","HPCC password (*Use JDBC client secure interface if available*)."});
+			put("username", 			new String[]{"hpccdemo","HPCC username (*Use JDBC client secure interface if available*)."});
+			put("WsECLDirectPort", 		new String[]{"8010","WsECLDirect port (required if HPCC configuration does not use default port)."});
+			put("WsECLPort", 			new String[]{"8002","WsECL port (required if HPCC configuration does not use default port)."});
+			put("WsECLWatchPort", 		new String[]{"8010","WsECLWatch port (required if HPCC configuration does not use default port)."});
+			put("ServerAddress", 		new String[]{DEFAULT_SERVER_ADDRESS, "Target HPCC ESP Address (used to contact WsECLWatch, WsECLDirect, or WsECL if override not specified)."});
+			put("WsECLWatchAddress", 	new String[]{DEFAULT_SERVER_ADDRESS, "WsECLWatch address (required if different than ServerAddress)."});
+			put("WsECLAddress", 		new String[]{DEFAULT_SERVER_ADDRESS, "WsECLAddress Address (required if different than ServerAddress)."});
+			put("WsECLDirectAddress", 	new String[]{DEFAULT_SERVER_ADDRESS, "WsECLDirect Address (required if different than ServerAddress)."});
+			put("Basic Auth", 			new String[]{HPCCConnection.createBasicAuth("hpccdemo", "hpccdemo"), "Basic Authentication is created at run-time level (from username and password)"});
+		}
+	};
+	
+	public HPCCDriverProperties(){
+		super();
+		this.initializeProperties();
+	}
+
+	public void initializeProperties() {
+		for(String key : defaultProperties.keySet()){
+			this.put(key, defaultProperties.get(key)[DEFAULT_VALUE_POSITION]);
+		}
+	}
+	
+	public Object put(Object key, Object value){
+		boolean isInDefaultProperies = defaultProperties.containsKey(key);
+		if(isInDefaultProperies){
+			super.put(key, value);
+		}
+		return isInDefaultProperies;
+	}
+	
+	public LinkedList<String> getAllPropertiesUsingServerAddress(){
+		LinkedList<String> propertiesWithServerAddress = new LinkedList<String>();
+		for(String key : defaultProperties.keySet()){
+			if(defaultProperties.get(key)[DEFAULT_VALUE_POSITION].equals(DEFAULT_SERVER_ADDRESS)){
+				propertiesWithServerAddress.add(key);
+			}
+		}
+		return propertiesWithServerAddress;
+	}
+	
+	public DriverPropertyInfo[] getProperties(){
+		return this.createPropertyInfo();
+	}
+	
+	private DriverPropertyInfo[] createPropertyInfo(){
+		
+		DriverPropertyInfo[] propertyInformation = new DriverPropertyInfo[this.size()];
+		int propertyCount = 0;
+		
+		for(String propertyName : defaultProperties.keySet()){
+			String propertyValue = (String) this.get(propertyName);
+			String defaultValue = defaultProperties.get(propertyName)[DEFAULT_VALUE_POSITION];
+			String propertyDescription = defaultProperties.get(propertyName)[DESCRIPTION_POSITION]+"(default: "+defaultValue+")";
+			
+			propertyInformation[propertyCount] = new DriverPropertyInfo((String)propertyName, propertyValue);
+			propertyInformation[propertyCount].description = propertyDescription;
+			propertyInformation[propertyCount].required = propertyName.equals("ServerAddress");
+			
+			if(propertyName.equals("LazyLoad")){
+				propertyInformation[propertyCount].choices = new String [] {"true", "false"};
+			}
+			
+			propertyCount++;
+		}
+		
+		return propertyInformation;
+	}	
+}
