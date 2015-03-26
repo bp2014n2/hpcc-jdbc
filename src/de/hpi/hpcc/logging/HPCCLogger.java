79d262c27765b11ea91503ded3c1b8fa25047408
peter.schwarz <pe.schwarz@hotmail.de>
2015-03-26 13:32:08 +0100

    show execeptions

--- src/de/hpi/hpcc/logging/HPCCLogger.java
+++ src/de/hpi/hpcc/logging/HPCCLogger.java
@@ -13,10 +13,10 @@ public class HPCCLogger extends Logger{
 	public static void initializeLogging(){
 		Logger logger = getLogger();
 		HPCCLogFormatter formatter = new HPCCLogFormatter();
-		logger.setUseParentHandlers(false);
+		logger.setUseParentHandlers(true);
 		try {
-			HPCCConsoleHandler handler = new HPCCConsoleHandler(formatter);
-			logger.addHandler(handler);
+//			HPCCConsoleHandler handler = new HPCCConsoleHandler(formatter);
+//			logger.addHandler(handler);
 		} catch (Exception exception){}
 		setLoggingLevel(getDefaultLogLevel());
 	}
