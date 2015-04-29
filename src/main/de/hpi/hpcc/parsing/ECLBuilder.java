b7d2466d497babebb1a3d8925f0ad535ee520153
Sven Lehmann <sven.lehmann@student.hpi.de>
2015-04-29 16:36:18 +0200

    removed class ECLFunction, adding JavaDoc

--- src/main/de/hpi/hpcc/parsing/ECLBuilder.java
+++ src/main/de/hpi/hpcc/parsing/ECLBuilder.java
@@ -7,13 +7,6 @@ import net.sf.jsqlparser.statement.Statement;
 abstract public class ECLBuilder {
 	protected ECLLayouts eclLayouts;
 	protected StringBuilder eclCode;
-	/**
-	 * This method generates ECL code from a given SQL code. 
-	 * Therefore it delegates the generation to the appropriate subclass, 
-	 * depending on the type of the given SQL (e.g. Select, Insert, Update, Drop or Create) 
-	 * @param sql
-	 * @return returns ECL code as String, including layout definitions and imports 
-	 */
 	
 	public ECLBuilder(Statement statement, ECLLayouts eclLayouts) {
 		this.eclLayouts = eclLayouts;
@@ -22,26 +15,20 @@ abstract public class ECLBuilder {
 	public abstract String generateECL();
 	
 	/**
-	 * This methods surrounds a given ECL string with a Table definition. 
-	 * The input StringBuilder is modified.
-	 * @param eclCode
+	 * @return returns the current SQLParser instance
 	 */
-	abstract protected SQLParser getSqlParser();
+	protected abstract SQLParser getSqlParser();
 	
 	/**
-	 * Generates for a given Expression the ECL code by a recursive approach
-	 * Each Expression can be e.g. a BinaryExpression (And, or etc. ), but also a Column or a SubSelect.
-	 * The break condition is accomplished if the Expression is a Column, Long or String
+	 * Generates for a given Expression the ECL code by using the appropriate visitor
+	 * Each Expression can be e.g. a BinaryExpression (and, or etc. ), but also a Column or a SubSelect.
 	 * @param expressionItem is the Expression under consideration
 	 * @return returns the ECL code for the given expression
 	 */
 	protected String parseExpressionECL(Expression expressionItem) {
-		StringBuilder expression = new StringBuilder();
 		ECLExpressionParser expressionParser = new ECLExpressionParser(eclLayouts);
 		expressionParser.setAllTables(getSqlParser().getAllTables());
-		expression.append(expressionParser.parse(expressionItem));
-		 
-		return expression.toString();
+		return expressionParser.parse(expressionItem);
 	}
 	
 }
