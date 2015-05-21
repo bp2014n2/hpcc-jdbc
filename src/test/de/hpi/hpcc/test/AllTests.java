19795933c9d9935a2407341f92b9df39dfbdd2d4
Sven Lehmann <sven.lehmann@student.hpi.de>
2015-05-21 18:50:56 +0200

    adding tests to test suite

--- src/test/de/hpi/hpcc/test/AllTests.java
+++ src/test/de/hpi/hpcc/test/AllTests.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ ECLBuilderTest.class, ECLBuilderInsertTest.class, ECLBuilderSelectTest.class, ECLColumnFinderTest.class, 
-	ECLDataTypeParserTest.class, ECLEngineTest.class, ECLSelectItemFinderTest.class, ECLTableFinderTest.class, 
-	HPCCDriverTest.class, HPCCUrlParserTest.class })
+	ECLDataTypeParserTest.class, ECLEngineTest.class, ECLSelectItemFinderTest.class, ECLSelectTableFinderTest.class,
+	ECLTableFinderTest.class, HPCCDriverTest.class, HPCCUrlParserTest.class })
 public class AllTests {
 
 }
